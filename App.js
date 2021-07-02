cc24c89029486a89f61343e2e049ca45b7481d7f
Aliu Raimi Olatunde <olatunderaimialiu@gmail.com>
2021-07-02 18:00:05 +0100

    environment port for loca git deployment

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 const {AppServiceHost} = require("./Program")
 
-const port = process.env.port || 3200
-const hostname = process.env.hostname || '127.0.0.1'
+// const port = process.env.port || 3200
+// const hostname = process.env.hostname || '127.0.0.1'
+const port = process.env.PORT
+const hostname = process.env.hostname
 // async function Start() {
 //     const app = await AppServiceHost(port, hostname)
 //     app.start()
@@ -9,5 +11,5 @@ const hostname = process.env.hostname || '127.0.0.1'
 
 // Start();
 
-const app = AppServiceHost(port, hostname)
+const app = AppServiceHost(port, 'localhost')
 app.start()
