fa1ad09d290d9cd8fa67fa2f3d0c8345c675b197
Raimi Tunde Aliu <32594255+raimialiu@users.noreply.github.com>
2021-07-09 06:05:45 +0100

    Update App.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const {AppServiceHost} = require("./Program")
 
 // const port = process.env.port || 3200
 // const hostname = process.env.hostname || '127.0.0.1'
-const port = process.env.PORT == null || process.env.PORT == undefined ? 3000 : process.env.PORT == null
+const port = process.env.PORT
 const hostname = process.env.hostname
 // async function Start() {
 //     const app = await AppServiceHost(port, hostname)
